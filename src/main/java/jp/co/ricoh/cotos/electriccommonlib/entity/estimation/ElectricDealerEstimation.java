7e04b2c963234681dcd21daa3687d9b7327089fc
chan-asa <Takahiro.Asakura@jrits.ricoh.co.jp>
2019-04-16 17:30:49 +0900

    見積エンティティの作成

---
+++ src/main/java/jp/co/ricoh/cotos/electriccommonlib/entity/estimation/ElectricDealerEstimation.java
@@ -0,0 +1,96 @@
+package jp.co.ricoh.cotos.electriccommonlib.entity.estimation;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import io.swagger.annotations.ApiModelProperty;
+import jp.co.ricoh.cotos.commonlib.entity.EntityBase;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 販売店情報
+ */
+@Entity
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Table(name = "electric_dealer_estimation")
+public class ElectricDealerEstimation extends EntityBase {
+
+/**
+ * ID
+ */
+@Id
+@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "electric_dealer_estimation_seq")
+@SequenceGenerator(name = "electric_dealer_estimation_seq", sequenceName = "electric_dealer_estimation_seq", allocationSize = 1)
+@ApiModelProperty(value = "ID", required = true, position = 1)
+private long id;
+
+/**
+ * 見積（電力）ID
+ */
+@OneToOne(optional = false)
+@JoinColumn(name = "estimationElectricId", referencedColumnName = "id")
+@JsonIgnore
+private EstimationElectric estimationElectric;
+
+/**
+ * 企業ID
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "企業ID", required = false, position = 2, allowableValues = "range[0,]")
+private String companyId;
+
+/**
+ * 事業所ID
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "事業所ID", required = false, position = 3, allowableValues = "range[0,]")
+private String officeId;
+
+/**
+ * 企事部ID
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "企事部ID", required = false, position = 4, allowableValues = "range[0,]")
+private String custId;
+
+/**
+ * 企業_事業所名
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "企業_事業所名", required = false, position = 5, allowableValues = "range[0,]")
+private String companyBusinessName;
+
+/**
+ * 事業所電話番号
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "事業所電話番号", required = false, position = 6, allowableValues = "range[0,]")
+private String phoneNumber;
+
+/**
+ * 郵便番号
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "郵便番号", required = false, position = 7, allowableValues = "range[0,]")
+private String postNumber;
+
+/**
+ * 住所
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "住所", required = false, position = 8, allowableValues = "range[0,]")
+private String address;
+
+}
+
---
+++ src/main/java/jp/co/ricoh/cotos/electriccommonlib/entity/estimation/FeeSimulationSales.java
@@ -0,0 +1,239 @@
+package jp.co.ricoh.cotos.electriccommonlib.entity.estimation;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import io.swagger.annotations.ApiModelProperty;
+import jp.co.ricoh.cotos.commonlib.entity.EntityBase;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 料金シミュレーション（営業用）
+ */
+@Entity
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Table(name = "fee_simulation_sales")
+public class FeeSimulationSales extends EntityBase {
+
+/**
+ * ID
+ */
+@Id
+@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "fee_simulation_sales_seq")
+@SequenceGenerator(name = "fee_simulation_sales_seq", sequenceName = "fee_simulation_sales_seq", allocationSize = 1)
+@ApiModelProperty(value = "ID", required = true, position = 1)
+private long id;
+
+/**
+ * 見積（電力）ID
+ */
+@OneToOne(optional = false)
+@JoinColumn(name = "estimationElectricId", referencedColumnName = "id")
+@JsonIgnore
+private EstimationElectric estimationElectric;
+
+/**
+ * SIM番号
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "SIM番号", required = false, position = 2, allowableValues = "range[0,]")
+private String simNumber;
+
+/**
+ * 作成日
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "作成日", required = false, position = 3)
+private Date createdDate;
+
+/**
+ * 年間電力料金 - 現状
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "年間電力料金 - 現状", required = false, position = 4)
+private BigDecimal annualElectricityRateCurrent;
+
+/**
+ * 年間電力料金 - 変更後
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "年間電力料金 - 変更後", required = false, position = 5)
+private BigDecimal annualElectricityRateAfter;
+
+/**
+ * 削減額
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "削減額", required = false, position = 6)
+private BigDecimal reductionAmount;
+
+/**
+ * 削減率（％）
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "削減率（％）", required = false, position = 7)
+private BigDecimal reductionRate;
+
+/**
+ * 総合単価１
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "総合単価１", required = false, position = 8)
+private BigDecimal overallPrice1;
+
+/**
+ * 総合単価２
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "総合単価２", required = false, position = 9)
+private BigDecimal overallPrice2;
+
+/**
+ * 総合単価３
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "総合単価３", required = false, position = 10)
+private BigDecimal overallPrice3;
+
+/**
+ * 負荷率（％）
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "負荷率（％）", required = false, position = 11)
+private BigDecimal loadFactor;
+
+/**
+ * 基本料金_売価
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "基本料金_売価", required = false, position = 12)
+private BigDecimal basicRateSellingPrice;
+
+/**
+ * 基本料金_仕切価格（営業）
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "基本料金_仕切価格（営業）", required = false, position = 13)
+private BigDecimal basicRateBankPriceBusiness;
+
+/**
+ * 従量料金_夏季_売価
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "従量料金_夏季_売価", required = false, position = 14)
+private BigDecimal usageFeeSummerSellingPrice;
+
+/**
+ * 従量料金_夏季_仕切価格（営業）
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "従量料金_夏季_仕切価格（営業）", required = false, position = 15)
+private BigDecimal usageFeeSummerBankPriceBusiness;
+
+/**
+ * 従量料金_その他季_売価
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "従量料金_その他季_売価", required = false, position = 16)
+private BigDecimal usageFeeOtherSeasonSellingPrice;
+
+/**
+ * 従量料金_その他季_仕切価格（営業）
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "従量料金_その他季_仕切価格（営業）", required = false, position = 17)
+private BigDecimal usageFeeOtherSeasonBankPriceBusiness;
+
+/**
+ * オプション_予備線_売価
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "オプション_予備線_売価", required = false, position = 18)
+private BigDecimal spareLineSellingPrice;
+
+/**
+ * オプション_予備線_仕切価格（営業）
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "オプション_予備線_仕切価格（営業）", required = false, position = 19)
+private BigDecimal spareLineBankPriceBusiness;
+
+/**
+ * オプション_予備電源_売価
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "オプション_予備電源_売価", required = false, position = 20)
+private BigDecimal sparePowerSellingPrice;
+
+/**
+ * オプション_予備電源_仕切価格（営業）
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "オプション_予備電源_仕切価格（営業）", required = false, position = 21)
+private BigDecimal sparePowerBankPriceBusiness;
+
+/**
+ * オプション_アンシラリー_売価
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "オプション_アンシラリー_売価", required = false, position = 22)
+private BigDecimal ancillarySellingPrice;
+
+/**
+ * オプション_アンシラリー_仕切価格（営業）
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "オプション_アンシラリー_仕切価格（営業）", required = false, position = 23)
+private BigDecimal ancillaryBankPriceBusiness;
+
+/**
+ * 媒介手数料　定額（円：税込）
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "媒介手数料　定額（円：税込）", required = false, position = 24)
+private BigDecimal feeFixedAmountInTax;
+
+/**
+ * 媒介手数料　定率（％）
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "媒介手数料　定率（％）", required = false, position = 25)
+private BigDecimal feeFixedRate;
+
+/**
+ * 電力料金（営業）
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "電力料金（営業）", required = false, position = 26)
+private BigDecimal electricityChargeBusiness;
+
+/**
+ * 粗利額（営業）
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "粗利額（営業）", required = false, position = 27)
+private BigDecimal grossMarginBusiness;
+
+/**
+ * 粗利率（％）（営業）
+ */
+@Column(nullable = false)
+@ApiModelProperty(value = "粗利率（％）（営業）", required = false, position = 28)
+private BigDecimal grossProfitMarginBusiness;
+
+}
+
