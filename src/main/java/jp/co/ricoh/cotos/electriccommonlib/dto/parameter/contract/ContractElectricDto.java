bf5946154bf04e9913936d37ebac319757dc0bc0
chan-asa <Takahiro.Asakura@jrits.ricoh.co.jp>
2019-05-13 10:57:05 +0900

    #2957 見積・契約の登録・更新用のDTOを作成（集約クラスのみ）

---
+++ src/main/java/jp/co/ricoh/cotos/electriccommonlib/dto/parameter/contract/ContractElectricDto.java
@@ -0,0 +1,12 @@
+package jp.co.ricoh.cotos.electriccommonlib.dto.parameter.contract;
+
+import jp.co.ricoh.cotos.commonlib.dto.parameter.common.DtoBase;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = false)
+@Data
+public class ContractElectricDto extends DtoBase {
+
+	// TODO: 更新系処理作成のタイミングで項目追加
+}
---
+++ src/main/java/jp/co/ricoh/cotos/electriccommonlib/dto/parameter/estimation/EstimationUpdateParameter.java
@@ -0,0 +1,21 @@
+package jp.co.ricoh.cotos.electriccommonlib.dto.parameter.estimation;
+
+import jp.co.ricoh.cotos.commonlib.dto.parameter.estimation.EstimationDto;
+import lombok.Data;
+
+/**
+ * 見積を登録・更新するためのキー項目クラスを表します。
+ */
+@Data
+public class EstimationUpdateParameter {
+
+	/**
+	 * 標準用見積DTO
+	 */
+	EstimationDto estimationDto;
+
+	/**
+	 * 電力用見積DTO
+	 */
+	EstimationElectricDto estimationElectricDto;
+}
