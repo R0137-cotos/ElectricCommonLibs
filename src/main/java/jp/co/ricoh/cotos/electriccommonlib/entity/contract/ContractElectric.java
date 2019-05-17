f644208ebf84a9c8dff2aba9e65811c08d87d178
Tatsuya Honma <tatsuya.honma@jrits.ricoh.co.jp>
2019-05-17 16:48:30 +0900

    CO2排出メニューのデータ型修正

--- src/main/java/jp/co/ricoh/cotos/electriccommonlib/entity/contract/ContractElectric.java
+++ src/main/java/jp/co/ricoh/cotos/electriccommonlib/entity/contract/ContractElectric.java
@@ -28,6 +28,7 @@ import jp.co.ricoh.cotos.commonlib.entity.contract.Contract.LifecycleStatus;
 import jp.co.ricoh.cotos.commonlib.security.complement.CotosComplementTarget;
 import jp.co.ricoh.cotos.electriccommonlib.entity.EnumType.ElectricCommercialFlowDiv;
 import jp.co.ricoh.cotos.electriccommonlib.entity.EnumType.VoltageCategory;
+import jp.co.ricoh.cotos.electriccommonlib.entity.master.ElectricFormMaster.ElectricPlan;
 import jp.co.ricoh.cotos.electriccommonlib.repository.contract.ContractElectricRepository;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -475,8 +476,8 @@ public class ContractElectric extends EntityBase {
 	 * CO2排出メニュー CO2EMISSION_MENUとして読み取られるためname指定
 	 */
 	@Column(nullable = true, name = "co2_emission_menu")
-	@ApiModelProperty(value = "CO2排出メニュー", required = false, position = 57, allowableValues = "range[0,255]")
-	private String co2EmissionMenu;
+	@ApiModelProperty(value = "CO2排出メニュー", required = false, position = 57, allowableValues = "CO2フリー(\"1\"), それ以外(\"2\")", example = "1")
+	private ElectricPlan co2EmissionMenu;
 
 	/**
 	 * 電力会社コード
