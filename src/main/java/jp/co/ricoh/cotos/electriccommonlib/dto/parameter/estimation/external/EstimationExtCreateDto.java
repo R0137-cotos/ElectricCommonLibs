a4edaa6d154977321978bc8b9ab7301ff98bcf6b
yamauchi <yasutaka.yamauchi@jrits.ricoh.co.jp>
2019-06-27 09:16:47 +0900

    見積新規作成用DTO作成

---
+++ src/main/java/jp/co/ricoh/cotos/electriccommonlib/dto/parameter/estimation/external/EstimationExtCreateDto.java
@@ -0,0 +1,87 @@
+package jp.co.ricoh.cotos.electriccommonlib.dto.parameter.estimation.external;
+
+import java.util.List;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = false)
+@Data
+public class EstimationExtCreateDto {
+
+	/**
+	 * 案件番号
+	 */
+	@NotNull
+	@Size(max = 255)
+	@ApiModelProperty(value = "案件番号", required = true, position = 4, allowableValues = "range[0,255]")
+	private String caseNumber;
+
+	/**
+	 * 案件名
+	 */
+	@NotNull
+	@Size(max = 255)
+	@ApiModelProperty(value = "案件名", required = true, position = 5, allowableValues = "range[0,255]")
+	private String caseTitle;
+
+	/**
+	 * 見積（電力用）
+	 */
+	@Valid
+	@NotNull
+	@ApiModelProperty(value = "見積（電力用）", required = true, position = 6)
+	private EstimationElectricExtDto estimationElectric;
+
+	/**
+	 * 顧客（見積用）
+	 */
+	@Valid
+	@NotNull
+	@ApiModelProperty(value = "顧客（見積用）", required = true, position = 7)
+	private CustomerEstimationExtDto customerEstimation;
+
+	/**
+	 * 見積担当SA社員
+	 */
+	@Valid
+	@NotNull
+	@ApiModelProperty(value = "見積担当SA社員", required = true, position = 8)
+	private EstimationPicSaEmpExtDto estimationPicSaEmp;
+
+	/**
+	 * 電力専任情報
+	 */
+	@Valid
+	@NotNull
+	@ApiModelProperty(value = "電力専任情報", required = true, position = 9)
+	private ElectricExpertEstimationExtDto electricExpertEstimation;
+
+	/**
+	 * 追加編集者
+	 */
+	@Valid
+	@ApiModelProperty(value = "追加編集者", required = false, position = 10)
+	private List<EstimationAddedEditorEmpExtDto> estimationAddedEditorEmpList;
+
+	/**
+	 * 販売店情報
+	 */
+	@Valid
+	@ApiModelProperty(value = "販売店情報", required = false, position = 11)
+	private ElectricDealerEstimationExtDto electricDealerEstimation;
+
+	/**
+	 * 料金シミュレーション（本部用）
+	 */
+	@Valid
+	@NotNull
+	@ApiModelProperty(value = "料金シミュレーション（本部用）", required = true, position = 12)
+	private FeeSimulationHeadExtDto feeSimulationHead;
+
+}
--- src/test/java/jp/co/ricoh/cotos/electriccommonlib/check/external/TestEstimationExtDto.java
+++ src/test/java/jp/co/ricoh/cotos/electriccommonlib/check/external/TestEstimationExtDto.java
@@ -23,6 +23,7 @@ import jp.co.ricoh.cotos.electriccommonlib.dto.parameter.estimation.external.Ele
 import jp.co.ricoh.cotos.electriccommonlib.dto.parameter.estimation.external.ElectricExpertEstimationExtDto;
 import jp.co.ricoh.cotos.electriccommonlib.dto.parameter.estimation.external.EstimationAddedEditorEmpExtDto;
 import jp.co.ricoh.cotos.electriccommonlib.dto.parameter.estimation.external.EstimationElectricExtDto;
+import jp.co.ricoh.cotos.electriccommonlib.dto.parameter.estimation.external.EstimationExtCreateDto;
 import jp.co.ricoh.cotos.electriccommonlib.dto.parameter.estimation.external.EstimationExtPlanChangeDto;
 import jp.co.ricoh.cotos.electriccommonlib.dto.parameter.estimation.external.EstimationPicSaEmpExtDto;
 import jp.co.ricoh.cotos.electriccommonlib.dto.parameter.estimation.external.FeeSimulationHeadExtDto;
@@ -90,6 +91,69 @@ public class TestEstimationExtDto {
 		target.setCaseTitle("test");
 		target.setCaseNumber("test");
 		target.setOriginContractId(1);
+		EstimationElectric entityEstimation = estimationElectricRepository.findOne(1L);
+		EstimationElectricExtDto targetEstimation = new EstimationElectricExtDto();
+		BeanUtils.copyProperties(entityEstimation, targetEstimation);
+		targetEstimation.setElectricCommercialFlowDivCode("1");
+		targetEstimation.setElectricCommercialFlowDiv(entityEstimation.getElectricCommercialFlowDiv().toString());
+		targetEstimation.setCo2EmissionMenu(ElectricPlan.CO2フリー);
+		targetEstimation.setCo2EmissionFactor("1");
+		targetEstimation.setVoltageCategory(entityEstimation.getVoltageCategory().toString());
+		targetEstimation.setSupplyStartScheduledDate("2019/05/31");
+		target.setEstimationElectric(targetEstimation);
+
+		CustomerEstimationExtDto targetCustomer = 顧客正常データ作成();
+		target.setCustomerEstimation(targetCustomer);
+
+		EstimationPicSaEmpExtDto targetEmp = new EstimationPicSaEmpExtDto();
+		targetEmp.setMomEmployeeId("test");
+		target.setEstimationPicSaEmp(targetEmp);
+
+		ElectricExpertEstimationExtDto targetExpert = new ElectricExpertEstimationExtDto();
+		targetExpert.setMomEmployeeId("test");
+		target.setElectricExpertEstimation(targetExpert);
+
+		FeeSimulationHead entitySimulation = estimationElectricRepository.findOne(1L).getFeeSimulationHead();
+		FeeSimulationHeadExtDto targetSimulation = new FeeSimulationHeadExtDto();
+		BeanUtils.copyProperties(entitySimulation, targetSimulation);
+		target.setFeeSimulationHead(targetSimulation);
+		target.getFeeSimulationHead().setCreatedDate("2019/05/31");
+		ParamterCheckResult result = testSecurityController.callParameterCheck(target, headersProperties, localServerPort);
+		testTool.assertValidationOk(result);
+
+		//異常系
+		target.setCaseTitle(null);
+		target.setCaseNumber(null);
+		target.setEstimationElectric(null);
+		target.setCustomerEstimation(null);
+		target.setEstimationPicSaEmp(null);
+		target.setElectricExpertEstimation(null);
+		target.setFeeSimulationHead(null);
+
+		result = testSecurityController.callParameterCheck(target, headersProperties, localServerPort);
+		Assert.assertTrue(result.getErrorInfoList().size() == 7);
+
+		target.setEstimationElectric(new EstimationElectricExtDto());
+		target.setCustomerEstimation(new CustomerEstimationExtDto());
+		target.setEstimationPicSaEmp(new EstimationPicSaEmpExtDto());
+		target.setElectricExpertEstimation(new ElectricExpertEstimationExtDto());
+		target.setFeeSimulationHead(new FeeSimulationHeadExtDto());
+		target.setElectricDealerEstimation(new ElectricDealerEstimationExtDto());
+		target.setEstimationAddedEditorEmpList(Arrays.asList(new EstimationAddedEditorEmpExtDto()));
+
+		result = testSecurityController.callParameterCheck(target, headersProperties, localServerPort);
+		Assert.assertTrue(result.getErrorInfoList().size() == 88);
+
+	}
+	
+	@Test
+	public void EstimationExtCreateDtoのテスト() {
+
+		EstimationExtCreateDto target = new EstimationExtCreateDto();
+
+		//正常系
+		target.setCaseTitle("test");
+		target.setCaseNumber("test");
 		EstimationElectric entityEstimation = estimationElectricRepository.findOne(1L);
 		EstimationElectricExtDto targetEstimation = new EstimationElectricExtDto();
 		BeanUtils.copyProperties(entityEstimation, targetEstimation);
