22fd76662d794aaa40f1f53e14a2b2881ac5ec62
Tatsuya Honma <tatsuya.honma@jrits.ricoh.co.jp>
2019-07-10 09:39:36 +0900

    粗利振替先マスタをリストで取得できるように修正

--- src/main/java/jp/co/ricoh/cotos/electriccommonlib/repository/master/ProfitTransferDepartmentMasterRepository.java
+++ src/main/java/jp/co/ricoh/cotos/electriccommonlib/repository/master/ProfitTransferDepartmentMasterRepository.java
@@ -1,5 +1,7 @@
 package jp.co.ricoh.cotos.electriccommonlib.repository.master;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,6 @@ import jp.co.ricoh.cotos.electriccommonlib.entity.master.ProfitTransferDepartmen
 @Repository
 public interface ProfitTransferDepartmentMasterRepository extends CrudRepository<ProfitTransferDepartmentMaster, Long> {
 	
-	public ProfitTransferDepartmentMaster findByAffiliationCode(String affiliationCode);
+	public List<ProfitTransferDepartmentMaster> findByAffiliationCode(String affiliationCode);
 
 }
