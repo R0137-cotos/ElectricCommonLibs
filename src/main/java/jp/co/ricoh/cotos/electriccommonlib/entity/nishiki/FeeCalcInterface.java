c34d18e19a2ffc1f9ea4191a783f88e8a2aab811
User Name <naoki.tsuchibuchi@jrits.ricoh.co.jp>
2019-07-16 10:16:13 +0900

    NISHIKI料金計算結果論理名修正

--- src/main/java/jp/co/ricoh/cotos/electriccommonlib/entity/nishiki/FeeCalcInterface.java
+++ src/main/java/jp/co/ricoh/cotos/electriccommonlib/entity/nishiki/FeeCalcInterface.java
@@ -256,14 +256,14 @@ public class FeeCalcInterface extends EntityBase {
 	private String uprcItemCd;
 
 	/**
-	 * 出力対象年月
+	 * 対象年月
 	 */
 	@Column(nullable = true)
 	@ApiModelProperty(value = "出力対象年月", required = false, position = 29)
 	private String outputTargetYm;
 
 	/**
-	 * 請求書出力フラグ
+	 * 請求書作成済みフラグ
 	 */
 	@Column(nullable = false)
 	@ApiModelProperty(value = "請求書出力フラグ", required = true, position = 30)
