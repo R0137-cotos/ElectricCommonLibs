c5a2858e754cd0371e5f3f618e676d781817df46
AnzaiTakeo <anzai-takeo@softcomm.co.jp>
2019-07-17 10:03:28 +0900

    不要なコードを削除

--- src/main/java/jp/co/ricoh/cotos/electriccommonlib/repository/contract/BillingHistoryRepository.java
+++ src/main/java/jp/co/ricoh/cotos/electriccommonlib/repository/contract/BillingHistoryRepository.java
@@ -17,9 +17,4 @@ public interface BillingHistoryRepository extends CrudRepository<BillingHistory,
 	@Query(value = "SELECT * FROM BILLING_HISTORY WHERE SEND_MY_RICOH = :SEND_MY_RICOH", nativeQuery = true)
 	public List<BillingHistory> searchBySendMyRicoh(@Param("SEND_MY_RICOH")String sendMyRicoh);	
 	
-	@Modifying
-	@Transactional
-	@Query(value = "UPDATE BILLING_HISTORY SET SEND_MY_RICOH = '1' WHERE ID = :ID", nativeQuery = true)
-	public void updateSendMyRicohById(@Param("ID")long id);
-	
 }
