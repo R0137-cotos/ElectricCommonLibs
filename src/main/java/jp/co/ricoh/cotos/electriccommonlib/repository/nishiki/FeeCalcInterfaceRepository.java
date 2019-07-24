5e181b71a6bfd1f04a0bea31af89d9fe17195bf5
AnzaiTakeo <anzai-takeo@softcomm.co.jp>
2019-07-24 14:17:00 +0900

    料金計算結果リポジトリに検索処理追加

--- src/main/java/jp/co/ricoh/cotos/electriccommonlib/repository/nishiki/FeeCalcInterfaceRepository.java
+++ src/main/java/jp/co/ricoh/cotos/electriccommonlib/repository/nishiki/FeeCalcInterfaceRepository.java
@@ -1,11 +1,18 @@
 package jp.co.ricoh.cotos.electriccommonlib.repository.nishiki;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import jp.co.ricoh.cotos.electriccommonlib.entity.nishiki.FeeCalcInterface;
 
 @Repository
 public interface FeeCalcInterfaceRepository extends CrudRepository<FeeCalcInterface, Long> {
+
+	@Query(value = "SELECT * FROM FEE_CALC_INTERFACE WHERE FEE_CALC_INTERFACE.CTCT_BN = :CTCT_BN", nativeQuery = true)
+	public List<FeeCalcInterface> searchByCtctBn(@Param("CTCT_BN") String ctctBn);
 
 }
