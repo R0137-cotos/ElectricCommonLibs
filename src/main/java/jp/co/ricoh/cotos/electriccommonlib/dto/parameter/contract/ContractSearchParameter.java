08a56e1f5e5daa11f68b9619e74d3cbdbf30b514
akihiro.sato.sc@jp.ricoh.com <akihiro.sato.sc@jp.ricoh.com>
2020-06-03 16:42:23 +0900

    コメント修正

--- src/main/java/jp/co/ricoh/cotos/electriccommonlib/dto/parameter/contract/ContractSearchParameter.java
+++ src/main/java/jp/co/ricoh/cotos/electriccommonlib/dto/parameter/contract/ContractSearchParameter.java
@@ -207,15 +207,15 @@ public class ContractSearchParameter {
 	/**
 	 * 電力会社
 	 */
-	@ApiParam(value = "電力会社:部分一致", required = false)
-	@ApiModelProperty(value = "電力会社:部分一致<br />条件入力時、最低2文字以上の入力とする。", required = false, allowableValues = "range[0,255]")
+	@ApiParam(value = "電力会社:前方一致", required = false)
+	@ApiModelProperty(value = "電力会社:前方一致<br />条件入力時、最低2文字以上の入力とする。", required = false, allowableValues = "range[0,255]")
 	private String likeSearchPowerCompany;
 
 	/**
 	 * 電力専任 - 氏名
 	 */
-	@ApiParam(value = "電力専任 - 氏名:部分一致", required = false)
-	@ApiModelProperty(value = "電力専任 - 氏名:部分一致<br />条件入力時、最低2文字以上の入力とする。", required = false, allowableValues = "range[0,255]")
+	@ApiParam(value = "電力専任 - 氏名:前方一致", required = false)
+	@ApiModelProperty(value = "電力専任 - 氏名:前方一致<br />条件入力時、最低2文字以上の入力とする。", required = false, allowableValues = "range[0,255]")
 	private String likeSearchElectricExpertName;
 
 	/**
