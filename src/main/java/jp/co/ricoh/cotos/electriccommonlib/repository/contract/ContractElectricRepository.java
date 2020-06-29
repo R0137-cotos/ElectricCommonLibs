0e14a236b5deb2a54c32d7b5eae4d6dad35f12fe
akihiro.sato.sc@jp.ricoh.com <akihiro.sato.sc@jp.ricoh.com>
2020-06-29 17:29:16 +0900

    外部キー重複チェック用のメソッドを追加

--- src/main/java/jp/co/ricoh/cotos/electriccommonlib/repository/contract/ContractElectricRepository.java
+++ src/main/java/jp/co/ricoh/cotos/electriccommonlib/repository/contract/ContractElectricRepository.java
@@ -1,5 +1,7 @@
 package jp.co.ricoh.cotos.electriccommonlib.repository.contract;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,8 @@ import jp.co.ricoh.cotos.electriccommonlib.entity.contract.ContractElectric;
 
 @Repository
 public interface ContractElectricRepository extends CrudRepository<ContractElectric, Long> {
-	
+
 	public ContractElectric findByContractId(Long contractId);
+
+	public List<ContractElectric> findByOppSysKeyBn(String oppSysKeyBn);
 }
