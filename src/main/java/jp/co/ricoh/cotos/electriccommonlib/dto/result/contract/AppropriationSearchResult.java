e9f1947e6da36c031054b334d056aa98d5e76484
SasayamaKatsuharu <katsuharu.sasayama@jp.ricoh.com>
2020-10-02 13:57:51 +0900

    計上実績取得結果Dtoに項目追加

--- src/main/java/jp/co/ricoh/cotos/electriccommonlib/dto/result/contract/AppropriationSearchResult.java
+++ src/main/java/jp/co/ricoh/cotos/electriccommonlib/dto/result/contract/AppropriationSearchResult.java
@@ -84,38 +84,98 @@ public class AppropriationSearchResult {
 	private BigDecimal proceedsInTax;
 
 	/**
+	 * 契約電力kW
+	 */
+	@ApiModelProperty(value = "契約電力kW", required = false, position = 12)
+	private BigDecimal basicPowerAmount;
+
+	/**
+	 * 使用電力夏季kWh
+	 */
+	@ApiModelProperty(value = "使用電力夏季kWh", required = false, position = 13)
+	private BigDecimal monthlyTotalpowerAmountSummer;
+
+	/**
+	 * 使用電力他季kWh
+	 */
+	@ApiModelProperty(value = "使用電力他季kWh", required = false, position = 14)
+	private BigDecimal monthlyTotalpowerAmountOtherSeason;
+
+	/**
+	 * 使用電力従量kWh
+	 */
+	@ApiModelProperty(value = "使用電力従量kWh", required = false, position = 15)
+	private BigDecimal monthlyTotalpowerAmountCommon;
+
+	/**
+	 * 仕入金額(税抜)
+	 */
+	@ApiModelProperty(value = "仕入金額(税抜)", required = false, position = 16)
+	private BigDecimal partitionTotalChargeOutTax;
+
+	/**
+	 * 仕入金額(税額)
+	 */
+	@ApiModelProperty(value = "仕入金額(税額)", required = false, position = 17)
+	private BigDecimal partitionTax;
+
+	/**
+	 * 仕入金額(税込)
+	 */
+	@ApiModelProperty(value = "仕入金額(税込)", required = false, position = 18)
+	private BigDecimal partitionTotalChargeInTax;
+
+	/**
 	 * 仲介区分
 	 */
-	@ApiModelProperty(value = "仲介区分", required = false, position = 12)
+	@ApiModelProperty(value = "仲介区分", required = false, position = 19)
 	private String paymentDiv;
 
 	/**
 	 * 手数料率
 	 */
-	@ApiModelProperty(value = "手数料率", required = false, position = 13)
+	@ApiModelProperty(value = "手数料率", required = false, position = 20)
 	private BigDecimal tradingCompanyFeeFixedRate;
 
 	/**
 	 * 手数料金額
 	 */
-	@ApiModelProperty(value = "手数料金額", required = false, position = 14)
+	@ApiModelProperty(value = "手数料金額", required = false, position = 21)
 	private BigDecimal tradingCompanyFeeFixedAmountInTax;
 
 	/**
 	 * 支社粗利
 	 */
-	@ApiModelProperty(value = "支社粗利", required = false, position = 15)
+	@ApiModelProperty(value = "支社粗利", required = false, position = 22)
 	private BigDecimal rjGrossProfit;
+
+	/**
+	 * SA粗利
+	 */
+	@ApiModelProperty(value = "SA粗利", required = false, position = 23)
+	private BigDecimal salesSectionGrossProfit;
+
+	/**
+	 * 本部粗利
+	 */
+	@ApiModelProperty(value = "本部粗利", required = false, position = 24)
+	private BigDecimal headofficeGrossProfit;
 
 	/**
 	 * 取次手数料金率
 	 */
-	@ApiModelProperty(value = "取次手数料金率", required = false, position = 16)
+	@ApiModelProperty(value = "取次手数料金率", required = false, position = 25)
 	private BigDecimal agencyFeeRate;
 
 	/**
 	 * 取次手数料金額
 	 */
-	@ApiModelProperty(value = "取次手数料金額", required = false, position = 17)
+	@ApiModelProperty(value = "取次手数料金額", required = false, position = 26)
 	private BigDecimal agencyFeeAmount;
+
+	/**
+	 * 供給年月
+	 */
+	@ApiModelProperty(value = "供給年月", required = false, position = 27)
+	private String supplyYearMonth;
 }
