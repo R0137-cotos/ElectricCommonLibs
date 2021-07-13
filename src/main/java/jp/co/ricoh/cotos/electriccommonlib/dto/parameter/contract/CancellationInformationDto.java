5056d75737f86b001242489f3493f6f1115e8639
akihiro.sato.sc@jp.ricoh.com <akihiro.sato.sc@jp.ricoh.com>
2021-07-13 14:00:14 +0900

    解約情報に（解約手続時点）需給（供給）期間 終了日を追加、手配結果登録パラメータに解約指定時刻を追加

--- src/main/java/jp/co/ricoh/cotos/electriccommonlib/dto/parameter/contract/CancellationInformationDto.java
+++ src/main/java/jp/co/ricoh/cotos/electriccommonlib/dto/parameter/contract/CancellationInformationDto.java
@@ -3,6 +3,7 @@ package jp.co.ricoh.cotos.electriccommonlib.dto.parameter.contract;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.DecimalMin;
@@ -164,4 +165,12 @@ public class CancellationInformationDto extends DtoBase {
 	@Temporal(TemporalType.DATE)
 	@ApiModelProperty(value = "手配結果登録日", required = false, position = 21)
 	private Date registerArrangedDate;
+
+	/**
+	 * （解約手続時点）需給（供給）期間 終了日
+	 */
+	@Column(nullable = true)
+	@Temporal(TemporalType.DATE)
+	@ApiModelProperty(value = "（解約手続時点）需給（供給）期間 終了日", required = false, position = 22)
+	private Date contractYmdEndAtCancellation;
 }
