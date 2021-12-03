2154eecafde8bb10f90743e14e7da6f0f57ef7e8
akihiro.sato.sc@jp.ricoh.com <akihiro.sato.sc@jp.ricoh.com>
2021-12-03 13:05:54 +0900

    電気事業者マスタ追加

---
+++ src/main/java/jp/co/ricoh/cotos/electriccommonlib/dto/result/master/ElectricProviderMasterSuggestDto.java
@@ -0,0 +1,46 @@
+package jp.co.ricoh.cotos.electriccommonlib.dto.result.master;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 電気事業者マスタのサジェスト入力候補を返すDTO
+ */
+@Entity
+@EqualsAndHashCode(callSuper = false)
+@Data
+public class ElectricProviderMasterSuggestDto {
+
+	@Id
+	@ApiModelProperty(value = "ID", required = true, position = 1, allowableValues = "range[0,9223372036854775807]")
+	private long id;
+
+	/**
+	 * 登録番号
+	 */
+	@ApiModelProperty(value = "登録番号", required = false, position = 2, allowableValues = "range[0,255]")
+	private String registrationNumber;
+
+	/**
+	 * 氏名又は名称
+	 */
+	@ApiModelProperty(value = "氏名又は名称", required = false, position = 3, allowableValues = "range[0,255]")
+	private String providerName;
+
+	/**
+	 * 氏名又は名称（半角）
+	 */
+	@ApiModelProperty(value = "登録番号（半角）", required = false, position = 4, allowableValues = "range[0,255]")
+	private String providerNameSingle;
+
+	/**
+	 * 氏名又は名称（全角）
+	 */
+	@ApiModelProperty(value = "氏名又は名称（全角）", required = false, position = 5, allowableValues = "range[0,255]")
+	private String providerNameMulti;
+
+}
